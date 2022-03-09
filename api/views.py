19ef81b6118065b528fa50b6575994887565e84a
裴振飞 <peizhenfei@cvte.com>
2022-03-09 18:05:41 +0800

    添加日志记录

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+import logging
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
@@ -7,6 +8,8 @@ from utils.wxwork import wechat_client
 from utils.alerter import alerter, ALERTID
 from api.serializers import TagSerializer
 from api.constants import APP_TAG_MAP
+
+logger = logging.getLogger('root')
 
 
 class WxworkTag(GenericViewSet):
@@ -29,4 +32,5 @@ class WxworkTag(GenericViewSet):
             response = wechat_client.tag.add_users(tag_id=tag_id, user_ids=[user_id])
             if response['errcode'] == 0 and response['errmsg'] == 'ok':
                 alerter.message.send_text(agent_id=ALERTID, user_ids=[user_id], content=f'{app} 应用权限已开通，请在企微工作台查看')
+                logger.info(f'wxwork app <{app}> add user <{user_id}> with tag <{tag_id}>')
         return Response({'status': 'success', 'message': 'tag add user success'})
